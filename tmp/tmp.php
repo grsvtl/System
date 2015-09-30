52ae28ae81456f19ea2ba488107ff76405f3dc03
grsvtl <grsvtl@gmail.com>
2015-09-30 16:40:18 +0300

    Create tmp.php

---
+++ tmp/tmp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Pro-Design.Org</title>
+<meta name="Keywords" content="Pro-Design.Org" />
+<meta name="Description" content="Pro-Design.Org" />
+</head>
+
+<body style="text-align:center;">
+<h1>Pro-Design.Org</h1>
+<p><strong>Tel.:</strong> +373 69 606-101</p>
+<p><strong>E-mail:</strong> grsvtl@gmail.com</p>
+</body>
+</html>
